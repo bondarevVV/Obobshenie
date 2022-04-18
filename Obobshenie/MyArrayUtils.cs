5dc9cb2bcbb3e2b393df0c684899fed3f3d8ecd7
bondarevVV <90005849+bondarevVV@users.noreply.github.com>
2022-04-18 13:58:52 +0300

    First commit

---
+++ Obobshenie/MyArrayUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obobshenie
+{
+    internal class MyArrayUtils<T>
+    {
+        public void Print(T[] mass) { }
+
+        public void Reverse(T[] mass) { }
+    }
+}
